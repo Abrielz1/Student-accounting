2c385654a4efa3a26fdf25421a2a3834b6561aea
Abriel <abrielz11@gmail.com>
2023-10-31 13:07:43 +0300

    command aliases was fixed

--- src/main/java/com/example/Student/accounting/service/StudentAccountService.java
+++ src/main/java/com/example/Student/accounting/service/StudentAccountService.java
@@ -27,20 +27,20 @@ public class StudentAccountService {
         return new Student(studentId++, firstName, lastname, Age);
     }
 
-    @ShellMethod(key = "c")
+    @ShellMethod(key = "create")
     public void createStudent(@ShellOption(value = "a") Integer amount) {
         for (int i = 0; i < amount; i++) {
             addStudent(createStudent((i + "aaaa"), (i + "bbbbb"), (byte) (18 + i)));
         }
     }
 
-    @ShellMethod(key = "a")
+    @ShellMethod(key = "add")
     public void addStudent(@ShellOption(value = "s") Student student) {
         log.info("Student with id {} was added", student.getId());
         heapOfStudents.put(student.getId(), student);
     }
 
-    @ShellMethod(key = "t")
+    @ShellMethod(key = "print")
     public void printStudentsList() {
         for (Map.Entry<Long, Student> i : heapOfStudents.entrySet()) {
             System.out.println("student id: " + i.getKey());
@@ -50,7 +50,7 @@ public class StudentAccountService {
         }
     }
 
-    @ShellMethod(key = "r")
+    @ShellMethod(key = "rem")
     public void removeStudent(Long id) {
         if (heapOfStudents.containsKey(id) && !heapOfStudents.isEmpty()) {
             log.info("Student with id {} was removed", id);
@@ -61,7 +61,7 @@ public class StudentAccountService {
         }
     }
 
-    @ShellMethod(key = "p")
+    @ShellMethod(key = "purge")
     public void purgeHeap() {
         log.info("Faculty was purged!");
         heapOfStudents.clear();
