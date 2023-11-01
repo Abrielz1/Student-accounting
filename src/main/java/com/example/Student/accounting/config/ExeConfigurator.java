e0e39977b57972194f34ec5490688e23ab866280
Abriel <abrielz11@gmail.com>
2023-11-01 16:18:17 +0300

    profiler was added

--- src/main/java/com/example/Student/accounting/config/ExeConfigurator.java
+++ src/main/java/com/example/Student/accounting/config/ExeConfigurator.java
@@ -2,13 +2,15 @@ package com.example.Student.accounting.config;
 
 import com.example.Student.accounting.service.Executor;
 import com.example.Student.accounting.service.StudentAccountService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ExeConfigurator {
 
-    @Bean
+    @Bean(name = "executor")
+    @ConditionalOnProperty(prefix = "autoload", name = "service", havingValue = "load")
     public Executor executorInit(StudentAccountService service) {
         return new Executor(service);
     }
